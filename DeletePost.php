c03ef1bf52f96ad75a2107efa8d7b799e4f927b9
Ian Yake <iangyake@gmail.com>
2018-10-28 20:49:00 -0500

    display checmarked post ids

--- DeletePost.php
+++ DeletePost.php
@@ -1,15 +1,26 @@
 <?php 
-$_post = $_POST["post_id1_ex"];
-echo $_post . "<br>";
-if ($_post == "on"){
-  echo "first post is checked";
+
+$_submit = $_POST["submit"];
+if ($_submit == "submit"){
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "i229y416", "kei3aeTh", "i229y416");
+  $query = "SELECT post_id FROM Posts";
+  if ($result = $mysqli->query($query)) {
+      
+    while ($row = $result->fetch_assoc()) {
+        $id = $row["post_id"];
+        
+        //if checkbox is checked for that post_id name
+        if ($_post = $_POST["$id"]){
+           echo "post_id " . $id . " deleted<br>";
+         }
+    }
+    
+      $result->free();
+  } else {
+    echo "Query failed: looping through post ids";
+  }
+  $mysqli->close();
 }
-
-
-
-
-
-echo getPostRows();
 
 
 function getPostRows(){
@@ -38,7 +49,49 @@ function getPost($row_num){
   if ($result = $mysqli->query($query)) {
       
       while ($row = $result->fetch_assoc()) {
-          echo $row["content"] . $row["author_id"];
+          echo $row["content"];
+      }
+      $result->free();
+  } else {
+    echo "Query failed";
+  }
+  $mysqli->close();
+}
+
+function getAuthor($row_num){
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "i229y416", "kei3aeTh", "i229y416");
+
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+  
+  $query = "SELECT * FROM Posts LIMIT $row_num,1";
+  if ($result = $mysqli->query($query)) {
+      
+      while ($row = $result->fetch_assoc()) {
+          echo $row["author_id"];
+      }
+      $result->free();
+  } else {
+    echo "Query failed";
+  }
+  $mysqli->close();
+}
+
+function getPostID($row_num){
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "i229y416", "kei3aeTh", "i229y416");
+
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+  
+  $query = "SELECT * FROM Posts LIMIT $row_num,1";
+  if ($result = $mysqli->query($query)) {
+      
+      while ($row = $result->fetch_assoc()) {
+          echo $row["post_id"];
       }
       $result->free();
   } else {
